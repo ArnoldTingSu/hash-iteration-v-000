f3c3dcf0589ead63bd2c52040d1b0f9d19a9940f
Arnold Su <arnoldtingsu@gmail.com>
2018-11-18 01:31:46 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,8 @@
 }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each do |common_tongue_name, years_around_the_sun|
+    puts "Happy Birthday #{common_tongue_name}! You are now #{years_around_the_sun} years old!"
   end
 end
 
